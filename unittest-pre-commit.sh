a0597594adfd8b821b147f1c798981e52399f797
kraktus <56031107+kraktus@users.noreply.github.com>
2020-09-01 18:49:31 +0100

    Add files via upload

---
+++ unittest-pre-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Called by "git commit" with no arguments.  The hook should
+# exit with non-zero status after issuing an appropriate message if
+# it wants to stop the commit.
+#
+# To enable this hook, rename this file to "pre-commit".
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+#Always synchronise test with commit 
+git add -u;
+python3 tests.py;
+if [ $? -ne 0 ]
+then
+   echo -e "${RED}TESTS FAILED${NC}"
+   exit 1
+fi
+echo -e "${GREEN}TESTS PASSED${NC}"
