bc6c3cc5f2c31806bd68fefa23de800331b584f3
kraktus <kraktus@users.noreply.github.com>
2023-09-06 14:17:57 +0200

    remove unused comment

--- zsh/cargo-alias.zsh
+++ zsh/cargo-alias.zsh
@@ -5,7 +5,6 @@
 # Never make an alias dependent of another one, only rename
 
 alias c="cargo"
-# alias cf="cargo fmt && taplo fmt --config ~/Github/scripts/.taplo.toml"
 cf() {
     cargo fmt
     if (( $? ))
